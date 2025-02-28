14de2b75745058e493cf208db5179826ab6e38a7
Hakeem <hakeemmuts255@gmail.com>
2025-02-28 12:03:26 +0300

    just minor day two updates bro

---
+++ practproject/products/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Products (models.Model):
+    name = models.CharField(max_length=200, null= False)
+    price = models.IntegerField(default=0.00)
+    description = models.TextField(max_length=500, null = False)
+    image = models.ImageField(upload_to='products/', null = False)
+    quantity = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__ (self):
+        return self.name
